89b952b30c9103418acf3037cf9e0e36096a2769
GABE SANCTI <116204587+GABESANCTI@users.noreply.github.com>
2025-05-21 19:39:03 -0400

    Gestão de Contas de usuário
    Criação da funcionalidade de login do Yapper!
    Agora é possivel realizar: Login e LogOut na plataforma, assim como realizar o cadastro de usuário
    O modelo utilizado para o user simples é o modelo padrão do Django,  permanecera este modelo durante as fases iniciais do projeto.

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 from .models import Yap
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User #modelo padrao do django de usuario, alterar depois (incluir-> display_name)
+        fields = ['username', 'email', 'password1', 'password2']
 
 class YapForm(forms.ModelForm):
     class Meta:
