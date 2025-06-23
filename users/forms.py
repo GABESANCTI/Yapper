57107f0fa50aeb11a4fb250b388df6f472eeae09
GABE SANCTI <116204587+GABESANCTI@users.noreply.github.com>
2025-06-23 13:13:00 -0400

    "ajustes no frontend react e atualização do requirements.txt"

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 #revisar o nome do campo senha e confirmar_senha, pois o Django já possui um campo senha padrão
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- yapper/settings.py
+++ yapper/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
     'default_filter_backends': (
     'django_filters.rest_framework.DjangoFilterBackend'),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10  # Quantos Yaps por page muahahahahaha
+    'PAGE_SIZE': 3  # Quantos Yaps por page muahahahahaha
 }
 
 MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware',
