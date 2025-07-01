1ab87d00c5029377c2be28e50b5134c6d39039bb
gabe <gabesancti@gmail.com>
2025-06-30 21:32:47 -0400

    Funcionalidade de edição de perfil

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/<str:username>/', views.user_profile, name='user_profile'),
     path('profile/<str:username>/', views.user_profile, name='user_profile'),
     path('profile/<str:username>/follow/', views.follow_user, name='follow_user'),
     path('profile/<str:username>/unfollow/', views.unfollow_user, name='unfollow_user'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
